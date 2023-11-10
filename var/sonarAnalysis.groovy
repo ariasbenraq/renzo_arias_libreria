8d5b28204a80005feaa94c4a51069fc31e91794f
Renzo Arias <78250051+ariasbenraq@users.noreply.github.com>
2023-11-10 16:56:12 -0500

    Delete var directory

--- var/sonarAnalysis.groovy
+++
@@ -1,9 +0,0 @@
-def call(boolean abortPipeline = false) {
-    def branchName = env.BRANCH_NAME // Obtener el nombre de la rama desde la variable de entorno
-
-    if (abortPipeline || branchName == 'master' || branchName.startsWith('hotfix')) {
-        error("QualityGate failed! Aborting the pipeline.")
-    } else {
-        echo "QualityGate passed. Continuing the pipeline."
-    }
-}
